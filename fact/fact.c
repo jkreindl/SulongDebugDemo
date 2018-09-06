21d855f7b0454ba7c19f511676af8905b9b11597
Jacob Kreindl <jacob.kreindl@jku.at>
2018-09-06 17:51:26 +0200

    Fix Bug in "fact" C-Extension

--- fact/fact.c
+++ fact/fact.c
@@ -7,11 +7,13 @@ int fact(int n) {
   return result;
 }
 
-int callFact(VALUE self, int n) {
-    return fact(n);
+VALUE callFact(VALUE self, VALUE num) {
+  int n = NUM2INT(num);
+  int result = fact(n);
+  return INT2NUM(result);
 }
 
 void Init_FactExt() {
-    VALUE FactExt = rb_define_module("FactExt");
-    rb_define_method(FactExt, "fact", &callFact, 1);
+  VALUE FactExt = rb_define_module("FactExt");
+  rb_define_method(FactExt, "fact", &callFact, 1);
 }
