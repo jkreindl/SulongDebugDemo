af2192a4995bc907c1b47473d8dcb4175b1bb67d
Jacob Kreindl <jacob.kreindl@jku.at>
2019-04-03 15:49:37 +0200

    Improve Postfix Calculator Demos

--- postfix_calculator_ruby/calc.rb
+++ postfix_calculator_ruby/calc.rb
@@ -1,25 +1,28 @@
 require_relative "CalcExt"
 include CalcExt
 
-for operand in ARGV
-    case operand
-    when '+'
-        CalcExt::doOp(CalcExt::OP_ADD)
-    when '-'
-        CalcExt::doOp(CalcExt::OP_SUB)
-    when '*'
-        CalcExt::doOp(CalcExt::OP_MUL)
-    when '/'
-        CalcExt::doOp(CalcExt::OP_DIV)
-    when '='
-        stack = CalcExt::doOp(CalcExt::OP_PRINT)
-        puts 'Stack Content (top-down):'
-        stack.each { |elt| puts elt.to_s }
-    else
-        num = Integer(operand)
-        CalcExt::pushNumber(num)
-    end
+def parse_number(operand)
+    num = Integer(operand)
+    CalcExt::pushNumber(num)
 end
-done = nil
+
+def print_stack()
+    stack = CalcExt::doOp(CalcExt::OP_PRINT)
+    puts 'Stack Content (top-down):'
+    stack.each { |elt| puts elt.to_s }
+end
+
+args = ARGV
+args.each { |operand|
+    case operand
+    when '+' then CalcExt::doOp(CalcExt::OP_ADD)
+    when '-' then CalcExt::doOp(CalcExt::OP_SUB)
+    when '*' then CalcExt::doOp(CalcExt::OP_MUL)
+    when '/' then CalcExt::doOp(CalcExt::OP_DIV)
+    when '=' then print_stack()
+    else          parse_number(operand)
+    end
+}
+
 result = CalcExt::getResult
 puts 'The result is: ' + result.to_s
